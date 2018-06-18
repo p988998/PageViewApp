10a95b66e3aac4ccc271695226e1e2e0faa602b0
Bigshrimp666 <pannijin@gmail.com>
2018-06-18 17:36:13 -0600

    view pager

--- app/src/main/java/com/apps/nijinpan/pageviewapp/MainActivity.java
+++ app/src/main/java/com/apps/nijinpan/pageviewapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.apps.nijinpan.pageviewapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import static com.apps.nijinpan.pageviewapp.PageFragment.newInstance;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //nothing
-        //123
+
+        ViewPager viewPager = (ViewPager)findViewById(R.id.view_pager);
+        viewPager.setAdapter(new NumberPagerAdapter(getSupportFragmentManager()));
+    }
+
+    private class NumberPagerAdapter extends FragmentPagerAdapter {
+        public NumberPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PageFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
     }
 }
